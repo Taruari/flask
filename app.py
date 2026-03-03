e31e24562842da05661450fb5c1bba861eeba7e9
Taruari <taruari3619r@gmail.com>
2026-03-03 18:18:44 +0530

    back-end connection
    models
    
    env set-up
    
    fix
    
    fix

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 # save this as app.py
+# save this as app.py
 from flask import Flask
+from routes.movies_bp import movies_bp
+from config import Config
+from extensions import db
+from sqlalchemy.sql import text
+
 
 app = Flask(__name__)
+app.config.from_object(Config)  # URL
+
+db.init_app(app)
+
+
+with app.app_context():
+    try:
+        result = db.session.execute(text("SELECT 1")).fetchall()
+        print("Connection successful:", result)
+    except Exception as e:
+        print("Error connecting to the database:", e)
 
 
 @app.route("/")
 def hello():
     return "<h1>Hello, World! 🎉 🔥</h1>"
+HTTP_NOT_FOUND = 404
+
+app.register_blueprint(movies_bp)
+
---
+++ config.py
@@ -0,0 +1,11 @@
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+print(environ.get("DATABASE_URL"))
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
